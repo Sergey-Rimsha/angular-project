ee83359006e811fa34c73cba29edc6bc56ce19c4
Sergey.rimsha.ru@gmail.com <Sergey.rimsha.ru@gmail.com>
2022-09-21 13:07:09 +0300

    httpClient api todoList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,20 @@ import { ParentComponent } from './parent/parent.component'
 import { Parent2Component } from './parent2/parent2.component'
 import { Paren3Component } from './paren3/paren3.component'
 import { ValueService } from './services/value.service'
+import { TodosComponent } from './todos/todos.component'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
-  declarations: [AppComponent, ChildComponent, ParentComponent, Parent2Component, Paren3Component],
-  imports: [BrowserModule, FormsModule],
-  providers: [ValueService],
+  declarations: [
+    AppComponent,
+    ChildComponent,
+    ParentComponent,
+    Parent2Component,
+    Paren3Component,
+    TodosComponent,
+  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+
+interface Todo {
+  addedDate: string
+  id: string
+  order: number
+  title: string
+}
+
+@Component({
+  selector: 'inst-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss'],
+})
+export class TodosComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  todos: Todo[] = []
+
+  ngOnInit(): void {
+    this.getTodos()
+  }
+
+  getTodos() {
+    this.http
+      .get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', {
+        withCredentials: true,
+        headers: {
+          'api-key': '31e5f258-a64c-4753-8a60-acee980643ae',
+        },
+      })
+      .subscribe(res => {
+        this.todos = res
+      })
+  }
+}

26502836cfc8ea189ffcf7d4e7ddf63cb7829a81
Sergey.rimsha.ru@gmail.com <Sergey.rimsha.ru@gmail.com>
2022-09-21 14:00:03 +0300

    httpClient api todoList delete and create todo

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -8,6 +8,13 @@ interface Todo {
   title: string
 }
 
+interface BaseResponse<T = {}> {
+  data: T
+  messages: string[]
+  fieldsErrors: string[]
+  resultCode: number
+}
+
 @Component({
   selector: 'inst-todos',
   templateUrl: './todos.component.html',
@@ -16,6 +23,15 @@ interface Todo {
 export class TodosComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
+  baseURL = 'https://social-network.samuraijs.com/api/1.1'
+
+  httpOptions = {
+    withCredentials: true,
+    headers: {
+      'api-key': '31e5f258-a64c-4753-8a60-acee980643ae',
+    },
+  }
+
   todos: Todo[] = []
 
   ngOnInit(): void {
@@ -23,15 +39,29 @@ export class TodosComponent implements OnInit {
   }
 
   getTodos() {
+    this.http.get<Todo[]>(`${this.baseURL}/todo-lists`, this.httpOptions).subscribe(res => {
+      this.todos = res
+    })
+  }
+
+  createTodo() {
+    const title = 'Angular Learn'
     this.http
-      .get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', {
-        withCredentials: true,
-        headers: {
-          'api-key': '31e5f258-a64c-4753-8a60-acee980643ae',
-        },
-      })
+      .post<BaseResponse<{ item: Todo }>>(`${this.baseURL}/todo-lists`, { title }, this.httpOptions)
       .subscribe(res => {
-        this.todos = res
+        this.todos.unshift(res.data.item)
       })
+  }
+
+  deleteTodo(todoId: string) {
+    this.http
+      .delete<BaseResponse<{}>>(`${this.baseURL}/todo-lists/${todoId} `, this.httpOptions)
+      .subscribe(() => {
+        this.todos = this.todos.filter(el => el.id !== todoId)
+      })
+  }
+
+  onClickHandlerDeleteTodo(id: string) {
+    this.deleteTodo(id)
   }
 }
